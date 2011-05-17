7b961b7912100bd57e706527836166c0b1a438c8
falcon-5 <showgo0829@gmail.com>
2011-05-17 23:22:35 +0900

    Add Address View

--- src/org/example/mapviewsample/MapViewSample.java
+++ src/org/example/mapviewsample/MapViewSample.java
@@ -1,11 +1,17 @@
 package org.example.mapviewsample;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -30,7 +36,7 @@ public class MapViewSample extends MapActivity {
         mapView.setBuiltInZoomControls(true);
 
         //プライベートクラスとして以降で定義したOverlayの具象クラスを生成
-        ConcreteOverlay overlay = new ConcreteOverlay();
+        ConcreteOverlay overlay = new ConcreteOverlay(this);
 
         //生成したOverlayを追加する
         List<Overlay> overlayList = mapView.getOverlays();
@@ -58,10 +64,13 @@ public class MapViewSample extends MapActivity {
     	//円を描画するための色情報
     	Paint mCirclePaint;
 
+    	//緯度経度情報と住所情報を相互変換する
+    	Geocoder mGeocoder;
+
     	/**
     	 * コンストラクタ
     	 */
-    	ConcreteOverlay()
+    	ConcreteOverlay(Context context)
     	{
     		mGeoPoint = null;
     		mCirclePaint = new Paint();
@@ -69,6 +78,9 @@ public class MapViewSample extends MapActivity {
     		mCirclePaint.setStyle(Paint.Style.FILL);
     		//図形を不透明の赤色に指定
     		mCirclePaint.setARGB(255, 255, 0, 0);
+
+    		//Geocoderを日本語環境でセット
+    		mGeocoder = new Geocoder(context, Locale.JAPAN);
     	}
 
     	/**
@@ -80,6 +92,46 @@ public class MapViewSample extends MapActivity {
     	{
     		//タップされた位置の経度緯度情報をメンバにセット
     		mGeoPoint = point;
+
+    		try
+    		{
+    			//画面上のTextViewのインスタンスを取得
+    			TextView textView = (TextView)findViewById(R.id.TextView01);
+
+    			//市町村名まで取得できたかどうか
+    			boolean success = false;
+
+    			//緯度経度から住所を取得
+    			List<Address> addressList = mGeocoder.getFromLocation(point.getLatitudeE6()/1E6, point.getLongitudeE6()/1E6, 5);
+
+    			//検索結果を順に処理
+    			for(Iterator<Address> it = addressList.iterator(); it.hasNext();)
+    			{
+    				Address address = it.next();
+
+    				//国名を取得
+    				String country = address.getCountryName();
+    				//都道府県
+    				String admin = address.getAdminArea();
+    				//市区町村
+    				String locality = address.getLocality();
+
+    				//市区町村名まで取得できていればTextViewを更新
+    				if(country != null && admin != null && locality != null)
+    				{
+    					textView.setText(country + admin + locality);
+    					success = true;
+    					break;
+    				}
+    			}
+
+    			//取得に失敗していればTextViewをエラー表記に変更
+    			if(!success) textView.setText("Error");
+
+    			//TextViewの再描画を行う
+    			textView.invalidate();
+    		}
+    		catch(Exception e){}
 
     		//スーパークラスのonTapを呼び出す
     		return super.onTap(point, mapView);
